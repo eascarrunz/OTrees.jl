c414a768f40eef6d8686aecdd28636f933fcbce7
earx <eascarrunz.dev@mailfence.com>
2022-07-25 06:51:37 -0400

    fix

--- src/write_newick.jl
+++ src/write_newick.jl
@@ -13,30 +13,42 @@ function _nwkout(nwk::IO, s)
     return nwk
 end
 
-function create_newick(nwk, vp::NodeView; brlengths=true)
-    nc = length(children(vp))
+function create_newick_fork(nwk, vp, viewlist; brlengths = true)
+    nc = length(viewlist)
     if nc > 0
         nwk = _nwkout(nwk, '(')
-        for (i, c) in enumerate(children(vp))
-            nwk = create_newick(nwk, c; brlengths=brlengths)
+        for (i, c) in enumerate(viewlist)
+            nwk = create_newick_fork(nwk, c, children(c); brlengths=brlengths)
             nwk = i < nc ? _nwkout(nwk, ',') : nwk
         end
         nwk = _nwkout(nwk, ')')
     end
     nwk = _nwkout(nwk, vp.node.label)
-    nwk = brlengths ? _nwkout(nwk, brlstring(brlength(vp))) : nwk
-
-    return nwk
+    nwk = brlengths ? _nwkout(nwk, brlstring(brlength(vp))) : nwk    
 end
 
-"""
-    newick(node; brlengths=true, fmt=nothing)
 
-Return a Newick string representation of the clade under a `node`. Will not include branch lengths if `brlengths` is set to `false`.
-"""
-newick(vp::NodeView; brlengths=true) = create_newick("", vp; brlengths=brlengths) * ";"
+_newick(vp::NodeView; brlengths=true) = create_newick_fork("", vp, children(vp); brlengths=brlengths)
 
-newick(tree::OTree; brlengths=true) = newick(OTrees.firstview(tree.anchor); brlengths=brlengths)
+
+"""
+newick(tree; brlengths=true)
+newick(node; brlengths=true)
+newick(nodeview; brlengths=true)
+
+Return a Newick string representation of a tree.
+
+If a `node` is given, it will be used as the root of the Newick string. Give a `nodeview` instead of a node in order to get the Newick string of a subtree.
+Will not include branch lengths if `brlengths` is set to `false`.
+"""
+newick(vp::NodeView; brlengths = true) = _newick(vp; brlengths=brlengths) * ';'
+function newick(o::ONode; brlengths = true)
+    vp  = firstview(o)
+    nwk = create_newick_fork("", vp, neighbours(vp); brlengths=brlengths)
+
+    return nwk * ';'
+end
+newick(tree::OTree; brlengths = true) = newick(tree.anchor; brlengths = brlengths)
 
 """
 """
